aa239bd41e4cee50658a45728c956ff235cddd1b
parkerbenoit <72704200+parkerbenoit@users.noreply.github.com>
2020-10-30 11:13:31 -0700

    restart

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,30 @@
 //Business Logic
 
-function inputArray () {
-  let numbers = [];
-  for (let index = 0; index =< x; index +-1) {
-    let words = index.toString();
+//function inputArray (x) {
+//  let numbers = [];
+//  for (let index = 0; index <= x; index +=1) {
+//    let words = index.toString();
 
-    if (words.includes(3)) {
-      numbers.push("Won't you be my neighbor?");
-    } else if (words.includes(2)) {
-      numbers.push("Boop");
-    } else if (words.includes(1)) {
-      numbers.push("Beep");
-    } else {
-      numbers.push(words);
-    }
-  return numbers;
-}
+//    if (words.includes(3)) {
+//      numbers.push("Won't you be my neighbor?");
+//    } else if (words.includes(2)) {
+//      numbers.push("Boop");
+//    } else if (words.includes(1)) {
+//      numbers.push("Beep");
+//    } else {
+//      numbers.push(words);
+//    }
+//  return numbers;
+//}
  //User Interface Logic
 
-$(document).ready(function() {
-  $("form#userinputs").submit(function(event) {
-    event.preventDefault();
-    var x = ($()).val());
-    var result = inputArray(x);
+//$(document).ready(function() {
+  //$("form#userinputs").submit(function(event) {
+    //event.preventDefault();
+    //let x = ($("input#number")).val());
+    //let result = inputArray(x);
 
-    $("outputPhrase").show();
-    $("outputNumbers").text(result);
-  });
-});
+    //$("outputPhrase").show();
+    //$("outputNumbers").text(result);
+  //});
+//});
