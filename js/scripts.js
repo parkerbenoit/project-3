71773b12861cdb5c7a2a9f294bd5a811b0fcd2e3
parkerbenoit <72704200+parkerbenoit@users.noreply.github.com>
2020-10-30 10:36:05 -0700

    numbers are correctly assigned to phrases

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,16 @@ function inputArray () {
   for (let index = 0; index =< x; index +-1) {
     let words = index.toString();
 
-
-
     if (words.includes(3)) {
       numbers.push("Won't you be my neighbor?");
     } else if (words.includes(2)) {
       numbers.push("Boop");
-    } else if (words.includes(3)) {
+    } else if (words.includes(1)) {
       numbers.push("Beep");
     } else {
       numbers.push(words);
     }
+  return numbers;
 }
  //User Interface Logic
 
@@ -26,6 +25,6 @@ $(document).ready(function() {
     var result = inputArray(x);
 
     $("outputPhrase").show();
-
+    $("outputNumbers").text(result);
   });
 });
