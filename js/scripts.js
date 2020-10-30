f1ea96458b125fe2e59ca1f3849704679e003134
parkerbenoit <72704200+parkerbenoit@users.noreply.github.com>
2020-10-30 10:35:41 -0700

    nothing works but I think I'm close?

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,31 @@
 //Business Logic
 
+function inputArray () {
+  let numbers = [];
+  for (let index = 0; index =< x; index +-1) {
+    let words = index.toString();
 
 
-//User Interface Logic
 
+    if (words.includes(3)) {
+      numbers.push("Won't you be my neighbor?");
+    } else if (words.includes(2)) {
+      numbers.push("Boop");
+    } else if (words.includes(3)) {
+      numbers.push("Beep");
+    } else {
+      numbers.push(words);
+    }
+}
+ //User Interface Logic
+
+$(document).ready(function() {
+  $("form#userinputs").submit(function(event) {
+    event.preventDefault();
+    var x = ($()).val());
+    var result = inputArray(x);
+
+    $("outputPhrase").show();
+
+  });
+});
